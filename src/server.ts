bd70854d164532ca4ebf14cbc60505dbb172b5e0
talllorenc <talllorenc@gmail.com>
2024-04-29 18:03:28 +0500

    2

--- src/server.ts
+++ src/server.ts
@@ -4,22 +4,16 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import http from "http";
-import path from "path";
 import cookieParser from "cookie-parser";
 import routes from "./routes/index";
 const { logger, toLog } = require('./middleware/logger')
 
 const app = express();
+
 app.use(logger);
-
-const allowedOrigins = [
-  "https://lorenc-base.vercel.app",
-  "http://localhost:3000"
-];
-
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: process.env.ALLOWED_ORIGIN,
     credentials: true,
   })
 );
@@ -28,11 +22,6 @@ app.use(cookieParser());
 app.use('/public', express.static('public'));
 app.use("/api", routes);
 
-const server = http.createServer(app);
-server.listen(8080, () => {
-  console.log("Server running on!");
-});
-
 const dbConnectionString = process.env.MONGO_URL;
 mongoose.Promise = Promise;
 mongoose.connect(dbConnectionString);
@@ -40,3 +29,8 @@ mongoose.connection.on("error", (error) =>{
   console.log(error)
   toLog(`${error.no}: ${error.code}\t${error.syscall}\t${error.hostname}`, 'mongoErrLog.log')
 })
+
+const server = http.createServer(app);
+server.listen(8080, () => {
+  console.log("🚀 Server running on!");
+});
